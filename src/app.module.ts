d429b76a8e9ddb1660d03eaa9efaebea52354f0e
Andre Luiz <andreluizkanaan222@gamil.com>
2020-09-22 17:38:36 -0300

    Developing

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import RepoModule from './repo.module';
+import * as ormOptions from '../ormconfig';
+
+import UserResolver from './resolvers/user.resolver';
+import MessageResolver from './resolvers/message.resolver';
+
+const graphQLImports = [UserResolver, MessageResolver];
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(ormOptions),
+    RepoModule,
+    ...graphQLImports,
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+      playground: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/resolvers/message.resolver.ts
@@ -0,0 +1,57 @@
+import {
+  Args,
+  Mutation,
+  Parent,
+  Query,
+  ResolveField,
+  Resolver,
+} from '@nestjs/graphql';
+import RepoService from '../repo.service';
+
+import Message from '../db/models/message.entity';
+import User from 'src/db/models/user.entity';
+
+import MessageInput from './input/message.input';
+
+@Resolver(() => Message)
+export default class MessageResolver {
+  constructor(private readonly repoService: RepoService) {}
+
+  @Query(() => [Message])
+  public async getMessages(): Promise<Message[]> {
+    return this.repoService.messageRepo.find();
+  }
+
+  @Query(() => Message, { nullable: true })
+  public async getMessagesFromUser(
+    @Args('userId') userId: number,
+  ): Promise<Message[]> {
+    return this.repoService.messageRepo.find({
+      where: {
+        userId,
+      },
+    });
+  }
+
+  @Query(() => Message, { nullable: true })
+  public async getMessage(@Args('id') id: number): Promise<Message> {
+    return this.repoService.messageRepo.findOne(id);
+  }
+
+  @Mutation(() => Message)
+  public async createMessage(
+    @Args('data') input: MessageInput,
+  ): Promise<Message> {
+    const message = this.repoService.messageRepo.create({
+      userId: input.userId,
+      content: input.content,
+    });
+
+    return this.repoService.messageRepo.save(message);
+  }
+
+  @ResolveField(() => User, { name: 'user' })
+  public async getUser(@Parent() parent: Message): Promise<User> {
+    return this.repoService.userRepo.findOne(parent.userId);
+  }
+}
